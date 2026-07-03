778cc9a79a2c2e04348cf70bd8fb75c7e12520a0
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2026-07-04 00:54:29 +0530

    add hero card effect

--- app/Components/Home/Hero.jsx
+++ app/Components/Home/Hero.jsx
@@ -5,34 +5,39 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <div>
-  <div className="flex justify-center items-center mt-20">
-<GlitchText
-  speed={1}
-  enableShadows
-  enableOnHover={false}
-  className='custom-class'
->
- Blits
-</GlitchText>
-<Image
+  <div className="mt-12 flex items-center justify-center gap-3 px-4 scale-75 sm:scale-90 origin-center">
+    <GlitchText
+      speed={1}
+      enableShadows
+      enableOnHover={false}
+      className="custom-class"
+    >
+      Blits
+    </GlitchText>
+    <Image
       src="/logo.png"
       alt="Picture of the author"
-      width={200}
-      height={200}
+      width={100}
+      height={110}
+      className="h-auto w-40 sm:w-23"
     />
     <GlitchText
-  speed={1}
-  enableShadows
-  enableOnHover={false}
-  className='custom-class'
->
- Pixelmedia
-</GlitchText>
+      speed={1}
+      enableShadows
+      enableOnHover={false}
+      className="custom-class"
+    >
+      Pixelmedia
+    </GlitchText>
+  </div>
+<div className="flex justify-center items-center text-center px-6">
+  <p className="text-dirty-yellow text-2xl font-bricolage font-normal max-w-3xl leading-relaxed">
+    We started as a college Instagram page. Today, were a full-fledged production agency creating bold, authentic, and unforgettable visual stories that leave a lasting impression.
+  </p>
 </div>
-
 <div style={{ height: '600px', position: 'relative' }}>
   <CircularGallery
-    bend={10}
+    bend={16}
     textColor="#ffffff"
     borderRadius={0.05}
     scrollEase={0.05}
