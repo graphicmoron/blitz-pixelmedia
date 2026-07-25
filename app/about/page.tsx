82b1270515d649fd89b85c6afece012aa72a8e43
thisissahaj <s21sahaj@gmail.com>
2026-07-26 01:10:21 +0530

    margins and videointro fixes

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='w-full h-screen flex items-center justify-center'>
+    <div>Development in progress...</div>
+    </section>
+  )
+}
+
+export default page
