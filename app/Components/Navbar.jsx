68ab1b9caab32d211c2f2ed716062cdf665427e5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2026-09-23 23:26:24 +0530

    update

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
         >
           <Image
             className=""
-            src="/logos/Blitz-logo-white.svg"
+            src="/logos/blitzlogowhite.png"
             alt="Blitz Pixel Media"
             width={100}
             height={100}
@@ -97,7 +97,7 @@ export default function Navbar() {
         >
           <Image
             className=""
-            src="/logos/Blitz-logo-white.svg"
+            src="/logos/blitzlogowhite.png"
             alt="Blitz Pixel Media"
             width={80}
             height={100}
